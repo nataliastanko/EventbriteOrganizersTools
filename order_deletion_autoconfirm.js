cf9f96e6fbd9ce1f8c14e45f97c0881f58154f6c
Natalia Stanko <nataliastanko@users.noreply.github.com>
2018-06-13 01:04:02 +0100

    Update order_deletion_autoconfirm.js

--- order_deletion_autoconfirm.js
+++ order_deletion_autoconfirm.js
@@ -3,15 +3,17 @@
 // @description  Organizer's tool; Autoconfirm Evenbrite event's (free) order deletion
 // @icon         https://cdn.evbstatic.com/s3-build/perm_001/609192/django/images/favicons/favicon.ico
 // @namespace    https://github.com/nataliastanko/
+// @contactURL   https://nataliastanko.com
 // @author       nataliastanko
-// @version      0.4
+// @version      0.5
 // @license      MIT
 // @copyright    2018, nataliastanko (https://github.com/nataliastanko/)
 // @match        https://*eventbrite.*order-delete*
 // @grant        none
 // @downloadURL  https://rawgit.com/nataliastanko/EventbriteOrganizersTools/master/order_deletion_autoconfirm.js
 // @updateURL    https://rawgit.com/nataliastanko/EventbriteOrganizersTools/master/order_deletion_autoconfirm.js
-// @noframes
+// @homepage     https://github.com/nataliastanko/EventbriteOrganizersTools/
+// @supportURL   https://github.com/nataliastanko/EventbriteOrganizersTools/issues
 // ==/UserScript==
     
 (function () {
