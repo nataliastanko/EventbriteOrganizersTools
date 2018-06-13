a50d77018ccbc9d17614737d3e378823410bb20d
Natalia Stanko <nataliastanko@users.noreply.github.com>
2018-06-13 01:09:03 +0100

    Update and rename orders_autodeletion.js to orders_autodeletion.user.js

--- orders_autodeletion.user.js
+++ orders_autodeletion.user.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @name         Evenbrite autodelete orders
-// @namespace    https://openuserjs.org/meta/nataliastanko/
 // @description  Organizer's tool; Autodelete all Evenbrite event's (free) orders
+// @icon         https://cdn.evbstatic.com/s3-build/perm_001/609192/django/images/favicons/favicon.ico
+// @namespace    https://github.com/nataliastanko/
+// @contactURL   https://nataliastanko.com
 // @author       nataliastanko
-// @version      0.2
+// @version      0.5
 // @license      MIT
-// @copyright    2018, nataliastanko (https://openuserjs.org/users/nataliastanko)
-// @match        https://*eventbrite.*reports*
-// @downloadURL  https://openuserjs.org/install/nataliastanko/Evenbrite_autodelete_orders.user.js
-// @updateURL    https://openuserjs.org/meta/nataliastanko/Evenbrite_autodelete_orders.meta.js
+// @copyright    2018, nataliastanko (https://github.com/nataliastanko/)
+// @match        https://*eventbrite.*order-delete*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/nataliastanko/EventbriteOrganizersTools/master/orders_autodeletion.user.js
+// @downloadURL  https://raw.githubusercontent.com/nataliastanko/EventbriteOrganizersTools/master/orders_autodeletion.user.js
+// @homepage     https://github.com/nataliastanko/EventbriteOrganizersTools/
+// @supportURL   https://github.com/nataliastanko/EventbriteOrganizersTools/issues
 // ==/UserScript==
 
 (function() {
